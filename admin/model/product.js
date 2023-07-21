770cbbbc405604f291413a62caadfb016c3bb309
phamtannien <13tannien@gmail.com>
2023-07-21 19:31:03 +0700

    [edit & update product]

--- admin/model/product.js
+++ admin/model/product.js
@@ -1,7 +1,11 @@
-function Product (_id, _name, _price, _img, _desc){
+function Product (_id, _name, _price, _img, _desc, _screen, _backCam, _frontCam, _type){
     this.id = _id;
     this.name = _name;
     this.price = _price;
     this.img = _img;
     this.desc = _desc;
+    this.screen = _screen;
+    this.backCamera = _backCam;
+    this.frontCamera = _frontCam;
+    this.type = _type;
 }
