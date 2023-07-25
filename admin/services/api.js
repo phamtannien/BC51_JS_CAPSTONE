36daedcc370af171c2190c831217cef4b6b2351a
phamtannien <13tannien@gmail.com>
2023-07-25 21:25:13 +0700

    [upload footer admin page]

--- admin/services/api.js
+++ admin/services/api.js
@@ -1,21 +1,21 @@
 function Service() {
     this.getListProductApi = function(){
         var promise = axios({
-            url: "https://64b78f6821b9aa6eb0785fd7.mockapi.io/product",
+            url: "https://64b78d6021b9aa6eb07853c6.mockapi.io/Products",
             method: "GET",
         });
         return promise
     }
      this.delProductApi = function(id){
         var promise = axios({
-            url:` https://64b78f6821b9aa6eb0785fd7.mockapi.io/product/${id}` ,
+            url:` https://64b78d6021b9aa6eb07853c6.mockapi.io/Products${id}` ,
             method: "DELETE",
         });
         return promise
      };
      this.addProductApi = function(product){
         var promise = axios({
-            url: "https://64b78f6821b9aa6eb0785fd7.mockapi.io/product" ,
+            url: "https://64b78d6021b9aa6eb07853c6.mockapi.io/Products" ,
             method: "POST",
             data: product
         });
@@ -23,14 +23,14 @@ function Service() {
      };
      this.getListProductById = function(id){
         var promise = axios({
-            url:` https://64b78f6821b9aa6eb0785fd7.mockapi.io/product/${id}` ,
+            url:`https://64b78d6021b9aa6eb07853c6.mockapi.io/Products/${id}` ,
             method: "GET",
         });
         return promise
      }
      this.updateProductApi = function(product){
         var promise = axios({
-            url: ` https://64b78f6821b9aa6eb0785fd7.mockapi.io/product/${product.id}` ,
+            url: `https://64b78d6021b9aa6eb07853c6.mockapi.io/Products/${product.id}` ,
             method: "PUT",
             data: product,
         });
